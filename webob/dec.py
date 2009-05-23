4e9a69dd106edcf95508cf05e24dff647fc9dce7
Ian Bicking <ianb@colorstudy.com>
2009-05-23 00:25:58 +0000

    Minor improvements to the decorator.  Also remove exception normalization

--- webob/dec.py
+++ webob/dec.py
@@ -1,17 +1,25 @@
 import webob
 import webob.exc
 
+__all__ = ['wsgify']
+
 class wsgify(object):
 
+    # The class to use for requests:
     RequestClass = webob.Request
-    ResponseClass = webob.Response
+    # Bind these attributes to the request object when the request
+    # comes in:
     request_attrs = {}
+    # Replaces the function with a WSGI application instead of just
+    # attaching .wsgi_app:
     force_wsgi = False
+    # If true, then **req.urlvars are added to the signature
     add_urlvars = False
+    # Positional arguments to add to the function call:
     add_call_args = ()
+    # Keyword arguments to add to the function call:
     add_keyword_args = {}
-    normalize_exceptions = False
-    normalize_3xx_exceptions = False
+    # When this is middleware that wraps an application:
     middleware_wraps = None
 
     def __init__(self, func=None, **kw):
@@ -40,14 +48,7 @@ class wsgify(object):
         if self.func is None:
             args = []
         else:
-            name = getattr(self.func, 'func_name', None)
-            if name is None:
-                name = repr(self.func)
-            else:
-                module = getattr(self.func, 'func_globals', {}).get('__name__')
-                if module and module != '__main__':
-                    name = '%s.%s' % (module, name)
-            args = [name]
+            args = [_func_name(self.func)]
         args.extend(['%s=%r' % (name, value) for name, value in sorted(self._instance_args.items())
                      if name != 'middleware_wraps'
                      and (name != 'add_keyword_args' or self.middleware_wraps is None)])
@@ -63,39 +64,34 @@ class wsgify(object):
             r += '(%s)' % ', '.join(args)
         return r
 
+    # To match @decorator:
     @property
     def undecorated(self):
         return self.func
 
-    def __call__(self, *args, **kw):
+    def __call__(self, req, *args, **kw):
         if self.func is None:
-            if not args or len(args) > 1 or kw:
+            func = req
+            if not func or args or kw:
                 raise TypeError('wsgiapp is unbound; you must call it with a function')
-            return self.__class__(args[0], **self._instance_args)
+            return self.__class__(func, **self._instance_args)
         elif self.force_wsgi:
-            if kw or len(args) != 2:
+            if kw or len(args) != 1:
                 raise TypeError(
-                    "Incorrect WSGI signature: *%r, **%r" % (args, kw))
-            return self.wsgi_app(*args, **kw)
+                    "Incorrect WSGI signature: %r(%s)" % (self, _format_args(req, *args, **kw)))
+            return self.wsgi_app(req, args[0])
         else:
-            return self.call(*args, **kw)
+            return self.call(req, *args, **kw)
 
-    def call(self, *args, **kw):
+    def call(self, req, *args, **kw):
         """Call the function like normal, normalizing the response if
         asked for
         """
-        if 'normalize_exceptions' in kw:
-            normalize_exceptions = kw.pop('normalize_exceptions')
-        else:
-            normalize_exceptions = self.normalize_exceptions
-        if 'normalize_3xx_exceptions' in kw:
-            normalize_3xx_exceptions = kw.pop('normalize_3xx_exceptions')
-        else:
-            normalize_3xx_exceptions = self.normalize_3xx_exceptions
-        resp = self.func(*args, **kw)
-        req = args[0]
-        resp = self.normalize_response(req, resp, normalize_exceptions=normalize_exceptions,
-                                       normalize_3xx_exceptions=normalize_3xx_exceptions)
+        try:
+            resp = self.func(req, *args, **kw)
+        except TypeError:
+            assert 0, '%r(%s)->%r' % (self.func, _format_args(req, *args, **kw), self.wsgi_app)
+        resp = self.normalize_response(req, resp)
         return resp
 
     def __get__(self, obj, type=None):
@@ -111,26 +107,26 @@ class wsgify(object):
         req.response.request = req
         # Hacky, but I think it improves the traceback: (?)
         if self.handle_exception:
-            handle_exception = Exception
+            handle_exception = Exception # Catches all (well, most) exceptions
         else:
-            handle_exception = None
+            handle_exception = None      # Catches no exceptions
         try:
             resp = self.apply(req)
         except webob.exc.HTTPException, resp:
-            pass
+            pass # the exception is the new response
         except handle_exception, e:
             resp = self.handle_exception(req, e)
             if resp is None:
                 raise
         resp = self.normalize_response(req, resp)
         if hasattr(resp, 'wsgi_app'):
+            # Allows chaining return values
             resp = resp.wsgi_app
         return resp(environ, start_response)
 
-    def normalize_response(self, req, resp,
-                           normalize_exceptions=False,
-                           normalize_3xx_exceptions=False):
+    def normalize_response(self, req, resp):
         if resp is None:
+            ## FIXME: I'm not sure this is a good idea?
             resp = req.response
         elif isinstance(resp, basestring):
             body = resp
@@ -138,20 +134,6 @@ class wsgify(object):
             resp.write(body)
         if resp is not req.response:
             resp = req.response.merge_cookies(resp)
-        if normalize_exceptions:
-            if hasattr(resp, 'wsgi_response'):
-                resp = resp.wsgi_response
-            status_int = int(resp.status.split(None, 1)[0])
-            if (status_int >= 400
-                or (normalize_3xx_exceptions and status_int >= 300)):
-                exc_class = webob.exc.status_map.get(
-                    status_int,
-                    webob.exc.WSGIHTTPException)
-                exc_resp = exc_class(
-                    code=resp.status,
-                    headerlist=resp.headerlist,
-                    app_iter=resp.app_iter)
-                raise exc_resp.exception
         return resp
 
     # Optional exception handler:
@@ -170,24 +152,18 @@ class wsgify(object):
         else:
             kw = {}
         if self.add_keyword_args:
-            new_kw = self.add_keyword_args.copy()
-            new_kw.update(kw)
-            kw = new_kw
+            kw.update(self.add_keyword_args)
         return self.call(*args, **kw)
 
     @classmethod
-    def reverse(cls, wsgi_app, **kw):
+    def reverse(cls, wsgi_app):
         """Takes a WSGI application and gives it a calling signature
         similar to a wrapped function (``resp = func(req)``)"""
         if hasattr(wsgi_app, 'wsgi_app'):
             wsgi_app = wsgi_app.wsgi_app
         def method(req):
-            status, headerlist, app_iter = req.call_application(wsgi_app)
-            return cls.ResponseClass(
-                status=status,
-                headerlist=headerlist,
-                app_iter=app_iter)
-        return cls(method, **kw)
+            return req.get_response(wsgi_app)
+        return cls(method)
 
     @classmethod
     def middleware(cls, middle_func=None, app=None, **kw):
@@ -204,22 +180,71 @@ class wsgify(object):
 
             app = set_user(app, username='bob')
         """
+        if middle_func is None:
+            return _UnboundMiddleware(cls, app, kw)
+        if app is None:
+            return _MiddlewareFactory(cls, middle_func, kw)
+        if hasattr(app, 'wsgi_app'):
+            app = app.wsgi_app
         if 'reverse_args' in kw:
             reverse_args = kw.pop('reverse_args')
         else:
             reverse_args = {}
-        if middle_func is None:
-            def middleware_decorator(func):
-                return cls.middleware(middle_func, app=app, **kw)
-            return middleware_decorator
-        if app is None:
-            def middleware_factory(app, **config):
-                new_kw = kw.copy()
-                new_kw['add_keyword_args'] = config
-                return cls.middleware(middle_func, app, **new_kw)
-            return middleware_factory
-        if hasattr(app, 'wsgi_app'):
-            app = app.wsgi_app
         app = cls.reverse(app, **reverse_args)
         return cls(middle_func, middleware_wraps=app, **kw)
 
+class _UnboundMiddleware(object):
+    def __init__(self, wrapper_class, app, kw):
+        self.wrapper_class = wrapper_class
+        self.app = app
+        self.kw = kw
+    def __repr__(self):
+        if self.app:
+            args = (self.app,)
+        else:
+            args = ()
+        return '%s.middleware(%s)' % (
+            self.wrapper_class.__name__,
+            _format_args(*args, **self.kw))
+    def __call__(self, func, app=None):
+        if app is not None:
+            app = self.app
+        return self.wrapper_class.middleware(func, app=app, **self.kw)
+
+class _MiddlewareFactory(object):
+    def __init__(self, wrapper_class, middleware, kw):
+        self.wrapper_class = wrapper_class
+        self.middleware = middleware
+        self.kw = kw
+    def __repr__(self):
+        return '%s.middleware(%s)' % (
+            self.wrapper_class.__name__,
+            _format_args(self.middleware, **self.kw))
+    def __call__(self, app, **config):
+        kw = self.kw.copy()
+        kw['add_keyword_args'] = config
+        return self.wrapper_class.middleware(self.middleware, app, **kw)
+
+def _func_name(func):
+    """Returns the string name of a function, or method, as best it can"""
+    name = getattr(func, 'func_name', None)
+    if name is None:
+        name = repr(func)
+    else:
+        name_self = getattr(func, 'im_self', None)
+        if name_self is not None:
+            name = '%r.%s' % (name_self, name)
+        else:
+            name_class = getattr(func, 'im_class', None)
+            if name_class is not None:
+                name = '%s.%s' % (name_class.__name__, name)
+        module = getattr(func, 'func_globals', {}).get('__name__')
+        if module and module != '__main__':
+            name = '%s.%s' % (module, name)
+    return name
+
+def _format_args(*args, **kw):
+    all = [repr(arg) for arg in args]
+    all.extend(['%s=%r' % (name, value) for name, value in sorted(kw.items())])
+    return ', '.join(all)
+    
