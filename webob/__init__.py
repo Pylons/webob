3bfe6ffb3348ac65c501e645b09127e8be6285ed
Chris McDonough <chrism@plope.com>
2009-10-28 15:19:44 -0400

    * Moved most ``Request`` behavior into a new class named   ``BaseRequest``.  The ``Request`` class is now a superclass for   ``BaseRequest`` and a simple mixin which manages   ``environ['webob.adhoc_attrs']`` when ``__setitem__``,   ``__delitem__`` and ``__getitem__`` are called.  This allows   framework developers who do not want the   ``environ['webob.adhoc_attrs']`` mutation behavior from   ``__setattr__``.  (chrism hg fork)

--- webob/__init__.py
+++ webob/__init__.py
@@ -531,7 +531,7 @@ def _serialize_accept(value, header_name, AcceptClass, NilClass):
 
 
 
-class Request(object):
+class BaseRequest(object):
 
     ## Options:
     default_charset = None
@@ -572,31 +572,6 @@ class Request(object):
                     raise TypeError(
                         "Unexpected keyword: %s=%r" % (name, value))
                 setattr(self, name, value)
-
-    def __setattr__(self, attr, value, DEFAULT=[]):
-        ## FIXME: I don't know why I need this guard (though experimentation says I do)
-        if getattr(self.__class__, attr, DEFAULT) is not DEFAULT or attr.startswith('_'):
-            object.__setattr__(self, attr, value)
-        else:
-            self.environ.setdefault('webob.adhoc_attrs', {})[attr] = value
-
-    def __getattr__(self, attr):
-        ## FIXME: I don't know why I need this guard (though experimentation says I do)
-        if attr in self.__class__.__dict__:
-            return object.__getattribute__(self, attr)
-        try:
-            return self.environ['webob.adhoc_attrs'][attr]
-        except KeyError:
-            raise AttributeError(attr)
-
-    def __delattr__(self, attr):
-        ## FIXME: I don't know why I need this guard (though experimentation says I do)
-        if attr in self.__class__.__dict__:
-            return object.__delattr__(self, attr)
-        try:
-            del self.environ['webob.adhoc_attrs'][attr]
-        except KeyError:
-            raise AttributeError(attr)
 
     def _body_file__get(self):
         """
@@ -1538,8 +1513,35 @@ class Request(object):
 
     blank = classmethod(blank)
 
+class AdhocAttrMixin(object):
+    def __setattr__(self, attr, value, DEFAULT=object()):
+        ## FIXME: I don't know why I need this guard (though experimentation says I do)
+        if getattr(self.__class__, attr, DEFAULT) is not DEFAULT or attr.startswith('_'):
+            object.__setattr__(self, attr, value)
+        else:
+            self.environ.setdefault('webob.adhoc_attrs', {})[attr] = value
 
+    def __getattr__(self, attr, DEFAULT=object()):
+        ## FIXME: I don't know why I need this guard (though experimentation says I do)
+        if getattr(self.__class__, attr, DEFAULT) is not DEFAULT:
+            return object.__getattribute__(self, attr)
+        try:
+            return self.environ['webob.adhoc_attrs'][attr]
+        except KeyError:
+            raise AttributeError(attr)
 
+    def __delattr__(self, attr, DEFAULT=object()):
+        ## FIXME: I don't know why I need this guard (though experimentation says I do)
+        if getattr(self.__class__, attr, DEFAULT) is not DEFAULT:
+            return object.__delattr__(self, attr)
+        try:
+            del self.environ['webob.adhoc_attrs'][attr]
+        except KeyError:
+            raise AttributeError(attr)
+    
+
+class Request(AdhocAttrMixin, BaseRequest):
+    """ The default request implementation """
 
 ################
 ################
