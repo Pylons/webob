42e93cb8306f5560e7b4c2fe7a68510e20d343ce
Ian Bicking <ianb@colorstudy.com>
2009-12-16 23:12:28 -0600

    update versions for 0.9.7 and remove some setup.cfg cruft

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ copyright = '2008, Ian Bicking'
 # The short X.Y version.
 version = '0.9'
 # The full version, including alpha/beta/rc tags.
-release = '0.9.6.1'
+release = '0.9.7'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
