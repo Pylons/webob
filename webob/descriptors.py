3cc02133dd3935704c1c44a0a1416b4195320aa7
Sergey Schetinin <sergey@maluke.com>
2010-01-05 22:59:38 +0200

    add more auth schemes

--- webob/descriptors.py
+++ webob/descriptors.py
@@ -424,10 +424,14 @@ def parse_params(params):
         r[key] = value.strip('"')
     return r
 
+# see http://lists.w3.org/Archives/Public/ietf-http-wg/2009OctDec/0297.html
+known_auth_schemes = ['Basic', 'Digest', 'WSSE', 'HMACDigest', 'GoogleLogin', 'Cookie', 'OpenID']
+known_auth_schemes = dict.fromkeys(known_auth_schemes, None)
+
 def parse_auth(val):
     if val is not None:
         authtype, params = val.split(' ', 1)
-        if authtype in ('Basic', 'Digest', 'WSSE'):
+        if authtype in known_auth_schemes:
             if authtype == 'Basic' and '"' not in params:
                 # this is the "Authentication: Basic XXXXX==" case
                 pass
