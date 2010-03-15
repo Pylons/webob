8bdf3aab8916845af2e240a2cf7776911607d96f
Sergey Schetinin <sergey@maluke.com>
2010-03-15 07:23:37 +0200

    expose part of Request.blank as environ_from_url func

--- webob/request.py
+++ webob/request.py
@@ -940,46 +940,7 @@ class BaseRequest(object):
         Any extra keyword will be passed to ``__init__`` (e.g.,
         ``decode_param_names``).
         """
-        if SCHEME_RE.search(path):
-            scheme, netloc, path, qs, fragment = urlparse.urlsplit(path)
-            if fragment:
-                raise TypeError(
-                    "Path cannot contain a fragment (%r)" % fragment)
-            if qs:
-                path += '?' + qs
-            if ':' not in netloc:
-                if scheme == 'http':
-                    netloc += ':80'
-                elif scheme == 'https':
-                    netloc += ':443'
-                else:
-                    raise TypeError("Unknown scheme: %r" % scheme)
-        else:
-            scheme = 'http'
-            netloc = 'localhost:80'
-        if path and '?' in path:
-            path_info, query_string = path.split('?', 1)
-            path_info = urllib.unquote(path_info)
-        else:
-            path_info = urllib.unquote(path)
-            query_string = ''
-        env = {
-            'REQUEST_METHOD': 'GET',
-            'SCRIPT_NAME': '',
-            'PATH_INFO': path_info or '',
-            'QUERY_STRING': query_string,
-            'SERVER_NAME': netloc.split(':')[0],
-            'SERVER_PORT': netloc.split(':')[1],
-            'HTTP_HOST': netloc,
-            'SERVER_PROTOCOL': 'HTTP/1.0',
-            'wsgi.version': (1, 0),
-            'wsgi.url_scheme': scheme,
-            'wsgi.input': StringIO(''),
-            'wsgi.errors': sys.stderr,
-            'wsgi.multithread': False,
-            'wsgi.multiprocess': False,
-            'wsgi.run_once': False,
-            }
+        env = environ_from_url(path)
         if POST is not None:
             env['REQUEST_METHOD'] = 'POST'
             if hasattr(POST, 'items'):
@@ -1020,6 +981,49 @@ class BaseRequest(object):
 
     blank = classmethod(blank)
 
+
+def environ_from_url(path):
+    if SCHEME_RE.search(path):
+        scheme, netloc, path, qs, fragment = urlparse.urlsplit(path)
+        if fragment:
+            raise TypeError(
+                "Path cannot contain a fragment (%r)" % fragment)
+        if qs:
+            path += '?' + qs
+        if ':' not in netloc:
+            if scheme == 'http':
+                netloc += ':80'
+            elif scheme == 'https':
+                netloc += ':443'
+            else:
+                raise TypeError("Unknown scheme: %r" % scheme)
+    else:
+        scheme = 'http'
+        netloc = 'localhost:80'
+    if path and '?' in path:
+        path_info, query_string = path.split('?', 1)
+        path_info = urllib.unquote(path_info)
+    else:
+        path_info = urllib.unquote(path)
+        query_string = ''
+    env = {
+        'REQUEST_METHOD': 'GET',
+        'SCRIPT_NAME': '',
+        'PATH_INFO': path_info or '',
+        'QUERY_STRING': query_string,
+        'SERVER_NAME': netloc.split(':')[0],
+        'SERVER_PORT': netloc.split(':')[1],
+        'HTTP_HOST': netloc,
+        'SERVER_PROTOCOL': 'HTTP/1.0',
+        'wsgi.version': (1, 0),
+        'wsgi.url_scheme': scheme,
+        'wsgi.input': StringIO(''),
+        'wsgi.errors': sys.stderr,
+        'wsgi.multithread': False,
+        'wsgi.multiprocess': False,
+        'wsgi.run_once': False,
+    }
+    return env
 
 
 class AdhocAttrMixin(object):
