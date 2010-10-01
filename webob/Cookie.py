bf56c2aa7701bbf72a827d22bdf7370207d471d8
Sergey Schetinin <sergey@maluke.com>
2010-10-01 15:15:18 +0300

    more refactoring

--- webob/Cookie.py
+++ webob/Cookie.py
@@ -6,107 +6,30 @@ _nulljoin = ''.join
 _semispacejoin = '; '.join
 _spacejoin = ' '.join
 
-# These quoting routines conform to the RFC2109 specification, which in
-# turn references the character definitions from RFC2068.  They provide
-# a two-way quoting algorithm.  Any non-text character is translated
-# into a 4 character sequence: a forward-slash followed by the
-# three-digit octal equivalent of the character.  Any '\' or '"' is
-# quoted with a preceeding '\' slash.
-#
-# These are taken from RFC2068 and RFC2109.
-#       _legal_chars       is the list of chars which don't require "'s
-#       _Translator       hash-table for fast quoting
-#
-_legal_chars       = string.ascii_letters + string.digits + "!#$%&'*+-.^_`|~"
-_Translator       = {
-    '\000' : '\\000',  '\001' : '\\001',  '\002' : '\\002',
-    '\003' : '\\003',  '\004' : '\\004',  '\005' : '\\005',
-    '\006' : '\\006',  '\007' : '\\007',  '\010' : '\\010',
-    '\011' : '\\011',  '\012' : '\\012',  '\013' : '\\013',
-    '\014' : '\\014',  '\015' : '\\015',  '\016' : '\\016',
-    '\017' : '\\017',  '\020' : '\\020',  '\021' : '\\021',
-    '\022' : '\\022',  '\023' : '\\023',  '\024' : '\\024',
-    '\025' : '\\025',  '\026' : '\\026',  '\027' : '\\027',
-    '\030' : '\\030',  '\031' : '\\031',  '\032' : '\\032',
-    '\033' : '\\033',  '\034' : '\\034',  '\035' : '\\035',
-    '\036' : '\\036',  '\037' : '\\037',
+_legal_chars = string.ascii_letters + string.digits + "!#$%&'*+-.^_`|~"
+_trans_noop = ''.join(chr(x) for x in xrange(256))
 
-    '"' : '\\"',       '\\' : '\\\\',
 
-    '\177' : '\\177',  '\200' : '\\200',  '\201' : '\\201',
-    '\202' : '\\202',  '\203' : '\\203',  '\204' : '\\204',
-    '\205' : '\\205',  '\206' : '\\206',  '\207' : '\\207',
-    '\210' : '\\210',  '\211' : '\\211',  '\212' : '\\212',
-    '\213' : '\\213',  '\214' : '\\214',  '\215' : '\\215',
-    '\216' : '\\216',  '\217' : '\\217',  '\220' : '\\220',
-    '\221' : '\\221',  '\222' : '\\222',  '\223' : '\\223',
-    '\224' : '\\224',  '\225' : '\\225',  '\226' : '\\226',
-    '\227' : '\\227',  '\230' : '\\230',  '\231' : '\\231',
-    '\232' : '\\232',  '\233' : '\\233',  '\234' : '\\234',
-    '\235' : '\\235',  '\236' : '\\236',  '\237' : '\\237',
-    '\240' : '\\240',  '\241' : '\\241',  '\242' : '\\242',
-    '\243' : '\\243',  '\244' : '\\244',  '\245' : '\\245',
-    '\246' : '\\246',  '\247' : '\\247',  '\250' : '\\250',
-    '\251' : '\\251',  '\252' : '\\252',  '\253' : '\\253',
-    '\254' : '\\254',  '\255' : '\\255',  '\256' : '\\256',
-    '\257' : '\\257',  '\260' : '\\260',  '\261' : '\\261',
-    '\262' : '\\262',  '\263' : '\\263',  '\264' : '\\264',
-    '\265' : '\\265',  '\266' : '\\266',  '\267' : '\\267',
-    '\270' : '\\270',  '\271' : '\\271',  '\272' : '\\272',
-    '\273' : '\\273',  '\274' : '\\274',  '\275' : '\\275',
-    '\276' : '\\276',  '\277' : '\\277',  '\300' : '\\300',
-    '\301' : '\\301',  '\302' : '\\302',  '\303' : '\\303',
-    '\304' : '\\304',  '\305' : '\\305',  '\306' : '\\306',
-    '\307' : '\\307',  '\310' : '\\310',  '\311' : '\\311',
-    '\312' : '\\312',  '\313' : '\\313',  '\314' : '\\314',
-    '\315' : '\\315',  '\316' : '\\316',  '\317' : '\\317',
-    '\320' : '\\320',  '\321' : '\\321',  '\322' : '\\322',
-    '\323' : '\\323',  '\324' : '\\324',  '\325' : '\\325',
-    '\326' : '\\326',  '\327' : '\\327',  '\330' : '\\330',
-    '\331' : '\\331',  '\332' : '\\332',  '\333' : '\\333',
-    '\334' : '\\334',  '\335' : '\\335',  '\336' : '\\336',
-    '\337' : '\\337',  '\340' : '\\340',  '\341' : '\\341',
-    '\342' : '\\342',  '\343' : '\\343',  '\344' : '\\344',
-    '\345' : '\\345',  '\346' : '\\346',  '\347' : '\\347',
-    '\350' : '\\350',  '\351' : '\\351',  '\352' : '\\352',
-    '\353' : '\\353',  '\354' : '\\354',  '\355' : '\\355',
-    '\356' : '\\356',  '\357' : '\\357',  '\360' : '\\360',
-    '\361' : '\\361',  '\362' : '\\362',  '\363' : '\\363',
-    '\364' : '\\364',  '\365' : '\\365',  '\366' : '\\366',
-    '\367' : '\\367',  '\370' : '\\370',  '\371' : '\\371',
-    '\372' : '\\372',  '\373' : '\\373',  '\374' : '\\374',
-    '\375' : '\\375',  '\376' : '\\376',  '\377' : '\\377'
-    }
+_escape_map = dict((chr(i), '\\%03o' % i) for i in xrange(256))
+_escape_map['"'] = '\\"'
+_escape_map['\\'] = '\\\\'
+_escape_char = _escape_map.get
 
-_idmap = ''.join(chr(x) for x in xrange(256))
+def needs_escaping(v):
+    return string.translate(v, _trans_noop, _legal_chars)
 
-def _quote(str, legal_chars=_legal_chars, idmap=_idmap, translate=string.translate):
-    # If the string does not need to be double-quoted,
-    # then just return the string.  Otherwise, surround
-    # the string in doublequotes and precede quote (with a \)
-    # special characters.
-    if "" == translate(str, idmap, legal_chars):
-        return str
-    else:
-        return '"' + _nulljoin(map(_Translator.get, str, str)) + '"'
+def _quote(v):
+    if needs_escaping(v):
+        return '"' + _nulljoin(map(_escape_char, v, v)) + '"'
+    return v
 
 _OctalPatt = re.compile(r"\\[0-3][0-7][0-7]")
 _QuotePatt = re.compile(r"[\\].")
 
 def _unquote(str):
-    # If there aren't any doublequotes,
-    # then there can't be any special characters.  See RFC 2109.
-    if  len(str) < 2:
+    if not str or str[0] != str[-1] != '"':
         return str
-    if str[0] != '"' or str[-1] != '"':
-        return str
-
-    # We have to assume that we must decode this string.
-    # Down to work.
-
-    # Remove the "s
     str = str[1:-1]
-
     # Check for special sequences.  Examples:
     #    \012 --> \n
     #    \"   --> "
@@ -199,11 +122,9 @@ class Morsel(dict):
         return K.lower() in _cookie_props
     # end isReservedKey
 
-    def set(self, key, val, coded_val,
-            legal_chars=_legal_chars,
-            idmap=_idmap, translate=string.translate):
+    def set(self, key, val, coded_val):
         assert key.lower() not in _cookie_props
-        if "" != translate(key, idmap, legal_chars):
+        if needs_escaping(key):
             return
         self.key = key
         self.value = val
@@ -289,10 +210,11 @@ class Cookie(dict):
             if K.lower() in _cookie_props:
                 if M:
                     M[K] = _unquote(V)
+            elif K[0] == '$':
+                continue
             else:
                 rval, cval = self.value_decode(V)
-                self._set(K, rval, cval)
-                M = self[K]
+                M = self._set(K, rval, cval)
 
 
     @staticmethod
@@ -324,6 +246,7 @@ class Cookie(dict):
         morsel = self.get(key, Morsel())
         morsel.set(key, real_value, coded_value)
         dict.__setitem__(self, key, morsel)
+        return morsel
 
     def __setitem__(self, key, value):
         """Dictionary style assignment."""
