c85d7b71aa38ccfea06f00d29380e31f6e99808b
Chris McDonough <chrism@plope.com>
2010-12-23 17:53:27 -0500

    Make previously failing test pass on Python < 2.5.

--- tests/test_exc.py
+++ tests/test_exc.py
@@ -2,10 +2,15 @@ import webob
 import webob.exc
 import webob.dec
 
+import sys
+
 @webob.dec.wsgify
 def method_not_allowed_app(req):
     if req.method != 'GET':
-        raise webob.exc.HTTPMethodNotAllowed()
+        if sys.version_info > (2,5):
+            raise webob.exc.HTTPMethodNotAllowed()
+        else:
+            raise webob.exc.HTTPMethodNotAllowed().exception
     return 'hello!'
 
 def test_exception_with_unicode_data():
