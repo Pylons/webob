81abb376a3444efa822ac1b352709383865a5ae5
Chris Shenton <chris@koansys.com>
2011-03-16 23:12:02 -0400

    Emacs doesn't like UTF-8 but loves utf-8

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 from datetime import timedelta
 from webob import cookies
 from nose.tools import ok_, assert_raises, eq_
--- tests/test_headers.py
+++ tests/test_headers.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 from webob import headers
 from nose.tools import ok_, assert_raises, eq_ as eq
 
