e88952561e6a5da2f0b7c31d876735b48f7f947a
Chris McDonough <chrism@plope.com>
2011-03-19 16:23:36 -0400

    make doctests run without dtopt

--- docs/doctests.py
+++ docs/doctests.py
@@ -2,14 +2,15 @@ import unittest
 import doctest
 
 def test_suite():
+    flags = doctest.ELLIPSIS|doctest.NORMALIZE_WHITESPACE
     return unittest.TestSuite((
-        doctest.DocFileSuite('test_request.txt'),
-        doctest.DocFileSuite('test_response.txt'),
-        doctest.DocFileSuite('test_dec.txt'),
-        doctest.DocFileSuite('do-it-yourself.txt'),
-        doctest.DocFileSuite('file-example.txt'),
-        doctest.DocFileSuite('index.txt'),
-        doctest.DocFileSuite('reference.txt'),
+        doctest.DocFileSuite('test_request.txt', optionflags=flags),
+        doctest.DocFileSuite('test_response.txt', optionflags=flags),
+        doctest.DocFileSuite('test_dec.txt', optionflags=flags),
+        doctest.DocFileSuite('do-it-yourself.txt', optionflags=flags),
+        doctest.DocFileSuite('file-example.txt', optionflags=flags),
+        doctest.DocFileSuite('index.txt', optionflags=flags),
+        doctest.DocFileSuite('reference.txt', optionflags=flags),
         ))
 
 if __name__ == '__main__':
