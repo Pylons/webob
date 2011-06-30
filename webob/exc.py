cf45150290c28eae139fa87da57c10d01159837c
Sergey Schetinin <sergey@maluke.com>
2011-06-30 16:14:11 +0300

    deprecate HTTPException.exception and remove some py2.4 support code

--- webob/exc.py
+++ webob/exc.py
@@ -133,10 +133,10 @@ plain-text and html messages for various clients as well as
 error-free substitution of environment variables and headers.
 
 
-The subclasses of :class:`~_HTTPMove` 
+The subclasses of :class:`~_HTTPMove`
 (:class:`~HTTPMultipleChoices`, :class:`~HTTPMovedPermanently`,
 :class:`~HTTPFound`, :class:`~HTTPSeeOther`, :class:`~HTTPUseProxy` and
-:class:`~HTTPTemporaryRedirect`) are redirections that require a ``Location`` 
+:class:`~HTTPTemporaryRedirect`) are redirections that require a ``Location``
 field. Reflecting this, these subclasses have two additional keyword arguments:
 ``location`` and ``add_slash``.
 
@@ -146,9 +146,9 @@ Parameters:
       to set the location immediately
 
     ``add_slash``
-      set to True to redirect to the same URL as the request, except with a 
+      set to True to redirect to the same URL as the request, except with a
       ``/`` appended
-    
+
 Relative URLs in the location will be resolved to absolute.
 
 References:
@@ -165,8 +165,7 @@ import sys
 import types
 from string import Template
 from webob import Response, Request, html_escape
-
-newstyle_exceptions = issubclass(Exception, object)
+from webob.descriptors import deprecated_property
 
 tag_re = re.compile(r'<.*?>', re.S)
 br_re = re.compile(r'<br.*?>', re.I|re.S)
@@ -191,36 +190,19 @@ def strip_tags(value):
     return value
 
 class HTTPException(Exception):
-    """
-    Exception used on pre-Python-2.5, where new-style classes cannot be used as
-    an exception.
-    """
-
     def __init__(self, message, wsgi_response):
         Exception.__init__(self, message)
-        self.__dict__['wsgi_response'] = wsgi_response
+        self.wsgi_response = wsgi_response
 
     def __call__(self, environ, start_response):
         return self.wsgi_response(environ, start_response)
 
+    # TODO: add warning=False in version 1.1
+    # TODO: remove in version 1.2
+    @property
     def exception(self):
         return self
-
-    exception = property(exception)
-
-    # for old style exceptions
-    if not newstyle_exceptions:  #pragma NO COVERAGE
-        def __getattr__(self, attr):
-            if not attr.startswith('_'):
-                return getattr(self.wsgi_response, attr)
-            else:
-                raise AttributeError(attr)
-
-        def __setattr__(self, attr, value):
-            if attr.startswith('_') or attr in ('args',):
-                self.__dict__[attr] = value
-            else:
-                setattr(self.wsgi_response, attr, value)
+    exception = deprecated_property(exception, 'exception', "Raise HTTP exceptions directly")
 
 class WSGIHTTPException(Response, HTTPException):
 
@@ -341,18 +323,11 @@ ${body}''')
             return self.generate_response(environ, start_response)
         return Response.__call__(self, environ, start_response)
 
+    @property
     def wsgi_response(self):
         return self
 
-    wsgi_response = property(wsgi_response)
 
-    def exception(self):
-        if newstyle_exceptions:
-            return self
-        else:
-            return HTTPException(self.detail, self)
-
-    exception = property(exception)
 
 class HTTPError(WSGIHTTPException):
     """
@@ -376,7 +351,7 @@ class HTTPRedirection(WSGIHTTPException):
 class HTTPOk(WSGIHTTPException):
     """
     Base class for the 200's status code (successful responses)
-    
+
     code: 200, title: OK
     """
     code = 200
@@ -392,7 +367,7 @@ class HTTPCreated(HTTPOk):
 
     This indicates that request has been fulfilled and resulted in a new
     resource being created.
-    
+
     code: 201, title: Created
     """
     code = 201
@@ -431,7 +406,7 @@ class HTTPNoContent(HTTPOk):
     This indicates that the server has fulfilled the request but does
     not need to return an entity-body, and might want to return updated
     metainformation.
-    
+
     code: 204, title: No Content
     """
     code = 204
@@ -445,7 +420,7 @@ class HTTPResetContent(HTTPOk):
     This indicates that the the server has fulfilled the request and
     the user agent SHOULD reset the document view which caused the
     request to be sent.
-    
+
     code: 205, title: Reset Content
     """
     code = 205
@@ -458,7 +433,7 @@ class HTTPPartialContent(HTTPOk):
 
     This indicates that the server has fulfilled the partial GET
     request for the resource.
-    
+
     code: 206, title: Partial Content
     """
     code = 206
@@ -525,7 +500,7 @@ class HTTPMultipleChoices(_HTTPMove):
     and agent-driven negotiation information is being provided so that
     the user can select a preferred representation and redirect its
     request to that location.
-    
+
     code: 300, title: Multiple Choices
     """
     code = 300
@@ -550,7 +525,7 @@ class HTTPFound(_HTTPMove):
 
     This indicates that the requested resource resides temporarily under
     a different URI.
-    
+
     code: 302, title: Found
     """
     code = 302
@@ -566,7 +541,7 @@ class HTTPSeeOther(_HTTPMove):
     This indicates that the response to the request can be found under
     a different URI and SHOULD be retrieved using a GET method on that
     resource.
-    
+
     code: 303, title: See Other
     """
     code = 303
@@ -593,7 +568,7 @@ class HTTPUseProxy(_HTTPMove):
 
     This indicates that the requested resource MUST be accessed through
     the proxy given by the Location field.
-    
+
     code: 305, title: Use Proxy
     """
     # Not a move, but looks a little like one
@@ -608,7 +583,7 @@ class HTTPTemporaryRedirect(_HTTPMove):
 
     This indicates that the requested resource resides temporarily
     under a different URI.
-    
+
     code: 307, title: Temporary Redirect
     """
     code = 307
@@ -640,7 +615,7 @@ class HTTPUnauthorized(HTTPClientError):
     subclass of :class:`~HTTPClientError`
 
     This indicates that the request requires user authentication.
-    
+
     code: 401, title: Unauthorized
     """
     code = 401
@@ -654,7 +629,7 @@ class HTTPUnauthorized(HTTPClientError):
 class HTTPPaymentRequired(HTTPClientError):
     """
     subclass of :class:`~HTTPClientError`
-    
+
     code: 402, title: Payment Required
     """
     code = 402
@@ -680,7 +655,7 @@ class HTTPNotFound(HTTPClientError):
 
     This indicates that the server did not find anything matching the
     Request-URI.
-    
+
     code: 404, title: Not Found
     """
     code = 404
@@ -711,7 +686,7 @@ class HTTPNotAcceptable(HTTPClientError):
     capable of generating response entities which have content
     characteristics not acceptable according to the accept headers
     sent in the request.
-    
+
     code: 406, title: Not Acceptable
     """
     code = 406
@@ -728,7 +703,7 @@ class HTTPProxyAuthenticationRequired(HTTPClientError):
 
     This is similar to 401, but indicates that the client must first
     authenticate itself with the proxy.
-    
+
     code: 407, title: Proxy Authentication Required
     """
     code = 407
@@ -741,7 +716,7 @@ class HTTPRequestTimeout(HTTPClientError):
 
     This indicates that the client did not produce a request within
     the time that the server was prepared to wait.
-    
+
     code: 408, title: Request Timeout
     """
     code = 408
@@ -755,7 +730,7 @@ class HTTPConflict(HTTPClientError):
 
     This indicates that the request could not be completed due to a
     conflict with the current state of the resource.
-    
+
     code: 409, title: Conflict
     """
     code = 409
@@ -769,7 +744,7 @@ class HTTPGone(HTTPClientError):
 
     This indicates that the requested resource is no longer available
     at the server and no forwarding address is known.
-    
+
     code: 410, title: Gone
     """
     code = 410
@@ -783,7 +758,7 @@ class HTTPLengthRequired(HTTPClientError):
 
     This indicates that the the server refuses to accept the request
     without a defined Content-Length.
-    
+
     code: 411, title: Length Required
     """
     code = 411
@@ -797,7 +772,7 @@ class HTTPPreconditionFailed(HTTPClientError):
     This indicates that the precondition given in one or more of the
     request-header fields evaluated to false when it was tested on the
     server.
-    
+
     code: 412, title: Precondition Failed
     """
     code = 412
@@ -825,7 +800,7 @@ class HTTPRequestURITooLong(HTTPClientError):
     This indicates that the server is refusing to service the request
     because the Request-URI is longer than the server is willing to
     interpret.
-    
+
     code: 414, title: Request-URI Too Long
     """
     code = 414
@@ -839,7 +814,7 @@ class HTTPUnsupportedMediaType(HTTPClientError):
     This indicates that the server is refusing to service the request
     because the entity of the request is in a format not supported by
     the requested resource for the requested method.
-    
+
     code: 415, title: Unsupported Media Type
     """
     code = 415
@@ -859,7 +834,7 @@ class HTTPRequestRangeNotSatisfiable(HTTPClientError):
     range-specifier values in this field overlap the current extent
     of the selected resource, and the request did not include an
     If-Range request-header field.
-    
+
     code: 416, title: Request Range Not Satisfiable
     """
     code = 416
@@ -872,7 +847,7 @@ class HTTPExpectationFailed(HTTPClientError):
 
     This indidcates that the expectation given in an Expect
     request-header field could not be met by this server.
-    
+
     code: 417, title: Expectation Failed
     """
     code = 417
@@ -885,7 +860,7 @@ class HTTPUnprocessableEntity(HTTPClientError):
 
     This indicates that the server is unable to process the contained
     instructions. Only for WebDAV.
-    
+
     code: 422, title: Unprocessable Entity
     """
     ## Note: from WebDAV
@@ -898,7 +873,7 @@ class HTTPLocked(HTTPClientError):
     subclass of :class:`~HTTPClientError`
 
     This indicates that the resource is locked. Only for WebDAV
-    
+
     code: 423, title: Locked
     """
     ## Note: from WebDAV
@@ -913,7 +888,7 @@ class HTTPFailedDependency(HTTPClientError):
     This indicates that the method could not be performed because the
     requested action depended on another action and that action failed.
     Only for WebDAV.
-    
+
     code: 424, title: Failed Dependency
     """
     ## Note: from WebDAV
@@ -957,7 +932,7 @@ class HTTPNotImplemented(HTTPServerError):
 
     This indicates that the server does not support the functionality
     required to fulfill the request.
-    
+
     code: 501, title: Not Implemented
     """
     code = 501
@@ -973,7 +948,7 @@ class HTTPBadGateway(HTTPServerError):
     This indicates that the server, while acting as a gateway or proxy,
     received an invalid response from the upstream server it accessed
     in attempting to fulfill the request.
-    
+
     code: 502, title: Bad Gateway
     """
     code = 502
@@ -986,7 +961,7 @@ class HTTPServiceUnavailable(HTTPServerError):
 
     This indicates that the server is currently unable to handle the
     request due to a temporary overloading or maintenance of the server.
-    
+
     code: 503, title: Service Unavailable
     """
     code = 503
@@ -1029,7 +1004,7 @@ class HTTPInsufficientStorage(HTTPServerError):
 
     This indicates that the server does not have enough space to save
     the resource.
-    
+
     code: 507, title: Insufficient Storage
     """
     code = 507
