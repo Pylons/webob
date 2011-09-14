97d2dc67fc688da5ed135b7b7da83f4544d80c0d
Sergey Schetinin <sergey@maluke.com>
2011-09-14 18:56:40 +0300

    adjust status code text

--- webob/util.py
+++ webob/util.py
@@ -58,7 +58,7 @@ status_reasons = {
     200: 'OK',
     201: 'Created',
     202: 'Accepted',
-    203: 'Non Authoritative Information',
+    203: 'Non-Authoritative Information',
     204: 'No Content',
     205: 'Reset Content',
     206: 'Partial Content',
