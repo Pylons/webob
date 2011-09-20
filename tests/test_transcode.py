b2af91028d34395f6c5e143630265287f6b096ea
Sergey Schetinin <sergey@maluke.com>
2011-09-20 15:41:11 +0300

    change test_transcode not to depend on webob's multipart body encoding

--- tests/test_transcode.py
+++ tests/test_transcode.py
@@ -10,31 +10,31 @@ from nose.tools import eq_
 #     #r = Response(str(dict(req.POST)))
 #     return r(env, sr)
 
+t1 = '--BOUNDARY\r\nContent-Disposition: form-data; name="a"\r\n\r\n\xea\xf3...\r\n--BOUNDARY--'
+t2 = '--BOUNDARY\r\nContent-Disposition: form-data; name="a"; filename="file"\r\n\r\n\xea\xf3...\r\n--BOUNDARY--'
+t3 = '--BOUNDARY\r\nContent-Disposition: form-data; name="a"; filename="\xea\xf3..."\r\n\r\nfoo\r\n--BOUNDARY--'
+
 def test_transcode():
     def tapp(env, sr):
         req = Request(env)
         v = req.POST[req.query_string]
         if hasattr(v, 'filename'):
-            print `v.filename`
             r = Response(u'%s\n%r' % (v.filename, v.value))
         else:
             r = Response(v)
         return r(env, sr)
     text = u'ку...'
     def test(post):
-        req = Request.blank('/?a', POST=post,
-            content_type='multipart/form-data',
-            charset='windows-1251'
-        )
+        req = Request.blank('/?a', POST=post)
+        req.environ['CONTENT_TYPE'] = 'multipart/form-data; charset=windows-1251; boundary=BOUNDARY'
         return req.get_response(transcode_mw(tapp))
 
-    r = test({'a': text.encode('cp1251')})
+    r = test(t1)
     eq_(r.text, text)
-    r = test({'a': ('file', text.encode('cp1251'))})
+    r = test(t2)
     eq_(r.text, u'file\n%r' % text.encode('cp1251'))
-#     r = test({'a': (text, 'foo')})
-#     eq_(r.text, u"%s\n'foo'" % text)
-
+    r = test(t3)
+    eq_(r.text, u"%s\n'foo'" % text)
 
     #req = Request.blank('/?a', POST={'a': ('file', text.encode('cp1251'))},
 
