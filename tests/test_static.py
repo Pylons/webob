a8112578e0cc1ee6178402370a1ae7a0a5ba99d5
Jonathan Ballet <jon@multani.info>
2012-03-24 10:09:28 +0700

    Improve the test coverage of webob.static.PkgResourcesApp

--- tests/test_static.py
+++ tests/test_static.py
@@ -5,6 +5,7 @@ from time import gmtime
 import os
 import shutil
 import string
+import sys
 import tarfile
 import unittest
 import zipfile
@@ -15,6 +16,20 @@ from webob import static
 from webob.compat import bytes_
 from webob.request import Request, environ_from_url
 from webob.response import Response
+
+
+def get_response(app, path='/', **req_kw):
+    """Convenient function to query an application"""
+    req = Request(environ_from_url(path), **req_kw)
+    return req.get_response(app)
+
+
+def create_file(content, *paths):
+    """Convenient function to create a new file with some content"""
+    path = os.path.join(*paths)
+    with open(path, 'wb') as fp:
+        fp.write(bytes_(content))
+    return path
 
 
 def test_dataapp():
@@ -75,36 +90,32 @@ class TestFileApp(unittest.TestCase):
     def tearDown(self):
         os.unlink(self.tempfile)
 
-    def _get_response(self, app, **kw):
-        return Request(environ_from_url('/'), **kw).\
-                get_response(app)
-
     def test_fileapp(self):
         app = static.FileApp(self.tempfile)
-        resp1 = self._get_response(app)
+        resp1 = get_response(app)
         eq_(resp1.content_type, 'text/x-python')
         eq_(resp1.charset, 'UTF-8')
         eq_(resp1.last_modified.timetuple(), gmtime(getmtime(self.tempfile)))
 
         app.update(force=True)
-        resp2 = self._get_response(app)
+        resp2 = get_response(app)
         eq_(resp2.content_type, 'text/x-python')
         eq_(resp2.last_modified.timetuple(), gmtime(getmtime(self.tempfile)))
 
-        resp3 = self._get_response(app, range=(7, 11))
+        resp3 = get_response(app, range=(7, 11))
         eq_(resp3.status_int, 206)
         eq_(tuple(resp3.content_range)[:2], (7, 11))
         eq_(resp3.last_modified.timetuple(), gmtime(getmtime(self.tempfile)))
         eq_(resp3.body, b'this')
     def test_unexisting_file(self):
         app = static.FileApp('/tmp/this/doesnt/exist')
-        self.assertEqual(404, self._get_response(app).status_int)
+        self.assertEqual(404, get_response(app).status_int)
 
     def test_allowed_methods(self):
         app = static.FileApp(self.tempfile)
 
         # Alias
-        resp = lambda method: self._get_response(app, method=method)
+        resp = lambda method: get_response(app, method=method)
 
         self.assertEqual(200, resp(method='GET').status_int)
         self.assertEqual(200, resp(method='HEAD').status_int)
@@ -127,28 +138,18 @@ class TestDirectoryApp(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.test_dir)
 
-    def _get_response(self, app, path='/', **kw):
-        return Request(environ_from_url(path), **kw).\
-                get_response(app)
-
-    def _create_file(self, content, *paths):
-        path = os.path.join(self.test_dir, *paths)
-        with open(path, 'wb') as fp:
-            fp.write(content)
-        return path
-
     def test_empty_directory(self):
         app = static.DirectoryApp(self.test_dir)
-        self.assertEqual(404, self._get_response(app).status_int)
-        self.assertEqual(404, self._get_response(app, '/foo').status_int)
+        self.assertEqual(404, get_response(app).status_int)
+        self.assertEqual(404, get_response(app, '/foo').status_int)
 
     def test_serve_file(self):
         app = static.DirectoryApp(self.test_dir)
-        self._create_file(bytes_('abcde'), 'bar')
-        self.assertEqual(404, self._get_response(app).status_int)
-        self.assertEqual(404, self._get_response(app, '/foo').status_int)
+        create_file('abcde', self.test_dir, 'bar')
+        self.assertEqual(404, get_response(app).status_int)
+        self.assertEqual(404, get_response(app, '/foo').status_int)
 
-        resp = self._get_response(app, '/bar')
+        resp = get_response(app, '/bar')
         self.assertEqual(200, resp.status_int)
         self.assertEqual(bytes_('abcde'), resp.body)
 
@@ -157,19 +158,19 @@ class TestDirectoryApp(unittest.TestCase):
         #   /TEST_DIR/
         #   /TEST_DIR/bar
         #   /TEST_DIR/foo/   <- serve this directory
-        self._create_file(bytes_('abcde'), 'bar')
+        create_file('abcde', self.test_dir, 'bar')
         serve_path = os.path.join(self.test_dir, 'foo')
         os.mkdir(serve_path)
         app = static.DirectoryApp(serve_path)
 
         # The file exists, but is outside the served dir.
-        self.assertEqual(403, self._get_response(app, '/../bar').status_int)
+        self.assertEqual(403, get_response(app, '/../bar').status_int)
 
     def test_file_app_arguments(self):
         app = static.DirectoryApp(self.test_dir, content_type='xxx/yyy')
-        self._create_file(bytes_('abcde'), 'bar')
+        create_file('abcde', self.test_dir, 'bar')
 
-        resp = self._get_response(app, '/bar')
+        resp = get_response(app, '/bar')
         self.assertEqual(200, resp.status_int)
         self.assertEqual('xxx/yyy', resp.content_type)
 
@@ -181,13 +182,13 @@ class TestDirectoryApp(unittest.TestCase):
 
         app = static.DirectoryApp(self.test_dir)
         app.make_fileapp = make_fileapp
-        self._create_file(bytes_('abcde'), 'bar')
+        create_file('abcde', self.test_dir, 'bar')
 
-        self._get_response(app, '/bar')
+        get_response(app, '/bar')
         self.assertTrue(make_fileapp.called)
 
     def test_must_serve_directory(self):
-        serve_path = self._create_file(bytes_('abcde'), self.test_dir, 'bar')
+        serve_path = create_file('abcde', self.test_dir, 'bar')
         self.assertRaises(AssertionError, static.DirectoryApp, serve_path)
 
 
@@ -202,19 +203,14 @@ class TestArchivedFilesApp(unittest.TestCase):
         # /DIR_ARCHIVE/bar/
         # /DIR_ARCHIVE/bar/baz  <- fgh
         self.dir_archive = tempfile.mkdtemp()
-        with open(os.path.join(self.dir_archive, 'foo'), 'w') as fp:
-            fp.write('abcde')
+        create_file('abcde', self.dir_archive, 'foo')
+
         os.mkdir(os.path.join(self.dir_archive, 'bar'))
-        with open(os.path.join(self.dir_archive, 'bar', 'baz'), 'w') as fp:
-            fp.write('fgh')
+        create_file('fgh', self.dir_archive, 'bar', 'baz')
 
     def tearDown(self):
         os.unlink(self.archive_name)
         shutil.rmtree(self.dir_archive)
-
-    def _get_response(self, app, path='/', **kw):
-        return Request(environ_from_url(path), **kw).\
-                get_response(app)
 
     def _create_archive(self, kind):
         # We change the current working directory to add files so that they are
@@ -243,22 +239,22 @@ class TestArchivedFilesApp(unittest.TestCase):
         self._create_archive(kind)
         app = static.ArchivedFilesApp(self.archive_name)
 
-        resp1 = self._get_response(app, '/foo')
+        resp1 = get_response(app, '/foo')
         self.assertEqual(200, resp1.status_int)
         self.assertEqual(bytes_('abcde'), resp1.body)
 
         # Unknow file
-        resp2 = self._get_response(app, '/unknown')
+        resp2 = get_response(app, '/unknown')
         self.assertEqual(200, resp2.status_int)
         self.assertEqual(bytes_(''), resp2.body)
 
         # Directory
-        resp3 = self._get_response(app, '/bar')
+        resp3 = get_response(app, '/bar')
         self.assertEqual(200, resp3.status_int)
         self.assertEqual(bytes_(''), resp3.body)
 
         # File in subdirectory
-        resp4 = self._get_response(app, '/bar/baz')
+        resp4 = get_response(app, '/bar/baz')
         self.assertEqual(200, resp4.status_int)
         self.assertEqual(bytes_('fgh'), resp4.body)
 
@@ -271,3 +267,65 @@ class TestArchivedFilesApp(unittest.TestCase):
     def test_unknown_archive(self):
         self.assertRaises(AssertionError,
                           static.ArchivedFilesApp, self.archive_name)
+
+
+class TestPkgResourcesApp(unittest.TestCase):
+
+    test_mod = 'webob_static_testmod'
+
+    def setUp(self):
+        self.dir_archive = tempfile.mkdtemp()
+        mod_dir = os.path.join(self.dir_archive, self.test_mod)
+        os.mkdir(mod_dir)
+        os.mkdir(os.path.join(mod_dir, 'plop'))
+
+        create_file("", mod_dir, '__init__.py')
+        create_file("jpeg file", mod_dir, 'foo.jpg', )
+        create_file("text file", mod_dir, 'plop', 'bar.txt')
+
+        sys.path.insert(0, self.dir_archive)
+
+    def tearDown(self):
+        sys.path.remove(self.dir_archive)
+        # pkg_resources access sys.modules to import the module. Since we change
+        # the directory for each test, we need to clear the cached reference or
+        # it will always try to load the module from the first directory used.
+        sys.modules.pop(self.test_mod)
+        shutil.rmtree(self.dir_archive)
+
+    def test_serve_files_from_module(self):
+        app = static.PkgResourcesApp(self.test_mod)
+
+        rep = get_response(app, '/foo.jpg')
+        self.assertEqual(200, rep.status_int)
+        self.assertEqual(bytes_('jpeg file'), rep.body)
+
+        rep = get_response(app, '/plop/bar.txt')
+        self.assertEqual(200, rep.status_int)
+        self.assertEqual(bytes_('text file'), rep.body)
+
+    def test_serve_missing_file(self):
+        app = static.PkgResourcesApp(self.test_mod)
+        rep = get_response(app, '/non-existent-file')
+        # XXX: this seems wrong, it should return 404
+        self.assertEqual(200, rep.status_int)
+        self.assertEqual(bytes_(''), rep.body)
+
+    def test_serve_directory(self):
+        app = static.PkgResourcesApp(self.test_mod)
+        rep = get_response(app, '/plop/')
+        # XXX: this seems wrong, it should return 404 or 405
+        self.assertEqual(200, rep.status_int)
+        self.assertEqual(bytes_(''), rep.body)
+
+    def test_serve_with_prefix(self):
+        app = static.PkgResourcesApp(self.test_mod, 'plop')
+
+        rep = get_response(app, '/foo.jpg')
+        # XXX: this seems wrong, it should return 404
+        self.assertEqual(200, rep.status_int)
+        self.assertEqual(bytes_(''), rep.body)
+
+        rep = get_response(app, '/bar.txt')
+        self.assertEqual(200, rep.status_int)
+        self.assertEqual(bytes_('text file'), rep.body)
