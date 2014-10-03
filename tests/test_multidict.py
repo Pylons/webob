cef148cd05dd83f21828e997a46a87cc176d8130
Jeff Dairiki <dairiki@dairiki.org>
2014-10-03 08:39:12 -0700

    Test that GetDict mutators update QUERY_STRING

--- tests/test_multidict.py
+++ tests/test_multidict.py
@@ -308,12 +308,14 @@ class NestedMultiDictTestCase(BaseDictTests, unittest.TestCase):
 class TestGetDict(BaseDictTests, unittest.TestCase):
     klass = multidict.GetDict
 
-    def _get_instance(self, **kwargs):
+    def _get_instance(self, environ=None, **kwargs):
+        if environ is None:
+            environ = {}
         if kwargs:
             data = multidict.MultiDict(kwargs)
         else:
             data = self.data.copy()
-        return self.klass(data, {})
+        return self.klass(data, environ)
 
     def test_inititems(self):
         #The first argument passed into the __init__ method
@@ -377,6 +379,60 @@ class TestGetDict(BaseDictTests, unittest.TestCase):
     def test_repr_with_password(self):
         d = self._get_instance(password='pwd')
         self.assertEqual(repr(d), "GET([('password', '******')])")
+
+    def test_setitem_updates_QUERY_STRING(self):
+        env = {}
+        d = self._get_instance(environ=env)
+        d['a'] = '2'
+        self.assertEqual(env['QUERY_STRING'], 'b=1&a=2')
+
+    def test_add_updates_QUERY_STRING(self):
+        env = {}
+        d = self._get_instance(environ=env)
+        d.add('a', '2')
+        self.assertEqual(env['QUERY_STRING'], 'a=%C3%A9&a=e&a=f&b=1&a=2')
+
+    def test_delitem_updates_QUERY_STRING(self):
+        env = {}
+        d = self._get_instance(environ=env)
+        del d['a']
+        self.assertEqual(env['QUERY_STRING'], 'b=1')
+
+    def test_clear_updates_QUERY_STRING(self):
+        env = {}
+        d = self._get_instance(environ=env)
+        d.clear()
+        self.assertEqual(env['QUERY_STRING'], '')
+
+    def test_setdefault_updates_QUERY_STRING(self):
+        env = {}
+        d = self._get_instance(environ=env)
+        d.setdefault('c', '2')
+        self.assertEqual(env['QUERY_STRING'], 'a=%C3%A9&a=e&a=f&b=1&c=2')
+
+    def test_pop_updates_QUERY_STRING(self):
+        env = {}
+        d = self._get_instance(environ=env)
+        d.pop('a')
+        self.assertEqual(env['QUERY_STRING'], 'a=e&a=f&b=1')
+
+    def test_popitem_updates_QUERY_STRING(self):
+        env = {}
+        d = self._get_instance(environ=env)
+        d.popitem()
+        self.assertEqual(env['QUERY_STRING'], 'a=%C3%A9&a=e&a=f')
+
+    def test_update_updates_QUERY_STRING(self):
+        env = {}
+        d = self._get_instance(environ=env)
+        d.update([('a', '2')])
+        self.assertEqual(env['QUERY_STRING'], 'b=1&a=2')
+
+    def test_extend_updates_QUERY_STRING(self):
+        env = {}
+        d = self._get_instance(environ=env)
+        d.extend([('a', '2')])
+        self.assertEqual(env['QUERY_STRING'], 'a=%C3%A9&a=e&a=f&b=1&a=2')
 
 class NoVarsTestCase(unittest.TestCase):
     klass = multidict.NoVars
