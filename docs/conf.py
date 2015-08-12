d39fff4a72a98c153eec5e7cb0d9fd71db234115
Bert JW Regeer <bertjw@regeer.org>
2015-08-12 14:52:24 -0600

    Move file to allow it to be excluded easily
    More cleanup has to happen here, but at least tox is happy now.

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'jsonrpc-example-code/*']
+exclude_patterns = ['_build', 'jsonrpc-example-code/*', 'file-example-code/*']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
