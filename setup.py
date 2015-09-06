094e83758450e2fc2c7b6afda50723d70bddab5f
Bert JW Regeer <bertjw@regeer.org>
2015-09-06 16:56:51 -0600

    Prepare for 1.5.0b0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ docs_extras = [
 
 setup(
     name='WebOb',
-    version='1.5.0a2',
+    version='1.5.0b0',
     description="WSGI request and response object",
     long_description=README + '\n\n' +  CHANGES,
     classifiers=[
