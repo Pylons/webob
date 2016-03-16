113ce85c398edb27f98f2c6e39263bfd91cba0a2
Bert JW Regeer <bertjw@regeer.org>
2016-03-15 21:56:35 -0600

    Continue dev on 1.7.x

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ docs_extras = [
 
 setup(
     name='WebOb',
-    version='1.6.0',
+    version='1.7.0dev0',
     description="WSGI request and response object",
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
