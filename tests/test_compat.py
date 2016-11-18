a01eb047b57424661397dbd16f7dc354676cb47e
Bert JW Regeer <bertjw@regeer.org>
2016-11-17 23:47:55 -0700

    Add some tests for compat cgi_FieldStorage
    This makes sure that we don't regress on our FieldStorage support across
    supported versions of Python

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -1,4 +1,8 @@
 import unittest
+from io import BytesIO
+
+import pytest
+import sys
 
 from webob.compat import text_type
 
@@ -46,3 +50,87 @@ class bytes_Tests(unittest.TestCase):
         result = self._callFUT(val, 'utf-8')
         self.assertTrue(isinstance(result, bytes))
         self.assertEqual(result, b'La Pe\xc3\xb1a')
+
+class Test_cgi_FieldStorage_Py3_tests(object):
+
+    def test_fieldstorage_not_multipart(self):
+        from webob.compat import cgi_FieldStorage
+
+        POSTDATA = b'{"name": "Bert"}'
+
+        env = {
+            'REQUEST_METHOD': 'POST',
+            'CONTENT_TYPE': 'text/plain',
+            'CONTENT_LENGTH': str(len(POSTDATA))
+        }
+        fp = BytesIO(POSTDATA)
+        fs = cgi_FieldStorage(fp, environ=env)
+        assert fs.list is None
+        assert fs.value == b'{"name": "Bert"}'
+
+    @pytest.mark.skipif(
+        sys.version_info < (3, 0),
+        reason="FieldStorage on Python 2.7 is broken, see "
+               "https://github.com/Pylons/webob/issues/293"
+    )
+    def test_fieldstorage_part_content_length(self):
+        from webob.compat import cgi_FieldStorage
+        BOUNDARY = "JfISa01"
+        POSTDATA = """--JfISa01
+Content-Disposition: form-data; name="submit-name"
+Content-Length: 5
+
+Larry
+--JfISa01"""
+        env = {
+            'REQUEST_METHOD': 'POST',
+            'CONTENT_TYPE': 'multipart/form-data; boundary={}'.format(BOUNDARY),
+            'CONTENT_LENGTH': str(len(POSTDATA))}
+        fp = BytesIO(POSTDATA.encode('latin-1'))
+        fs = cgi_FieldStorage(fp, environ=env)
+        assert len(fs.list) == 1
+        assert fs.list[0].name == 'submit-name'
+        assert fs.list[0].value == b'Larry'
+
+    def test_fieldstorage_multipart_leading_whitespace(self):
+        from webob.compat import cgi_FieldStorage
+
+        BOUNDARY = "---------------------------721837373350705526688164684"
+        POSTDATA = """-----------------------------721837373350705526688164684
+Content-Disposition: form-data; name="id"
+
+1234
+-----------------------------721837373350705526688164684
+Content-Disposition: form-data; name="title"
+
+
+-----------------------------721837373350705526688164684
+Content-Disposition: form-data; name="file"; filename="test.txt"
+Content-Type: text/plain
+
+Testing 123.
+
+-----------------------------721837373350705526688164684
+Content-Disposition: form-data; name="submit"
+
+ Add\x20
+-----------------------------721837373350705526688164684--
+"""
+
+        env = {
+            'REQUEST_METHOD': 'POST',
+            'CONTENT_TYPE': 'multipart/form-data; boundary={}'.format(BOUNDARY),
+            'CONTENT_LENGTH': '560'}
+        # Add some leading whitespace to our post data that will cause the
+        # first line to not be the innerboundary.
+        fp = BytesIO(b"\r\n" + POSTDATA.encode('latin-1'))
+        fs = cgi_FieldStorage(fp, environ=env)
+        assert len(fs.list) == 4
+        expect = [{'name':'id', 'filename':None, 'value':'1234'},
+                  {'name':'title', 'filename':None, 'value':''},
+                  {'name':'file', 'filename':'test.txt', 'value':b'Testing 123.\n'},
+                  {'name':'submit', 'filename':None, 'value':' Add '}]
+        for x in range(len(fs.list)):
+            for k, exp in expect[x].items():
+                got = getattr(fs.list[x], k)
+                assert got == exp
