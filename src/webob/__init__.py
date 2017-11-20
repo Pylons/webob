ca50d1acd4e303e4d97ac6af8a4d067f05fb77b9
Bert JW Regeer <bertjw@regeer.org>
2017-11-20 14:53:39 -0700

    Explicitly import all the things, instead of doing import *

--- src/webob/__init__.py
+++ src/webob/__init__.py
@@ -1,6 +1,20 @@
-from webob.datetime_utils import *
-from webob.request import *
-from webob.response import *
+from webob.datetime_utils import (  # noqa: F401
+    UTC,
+    day,
+    hour,
+    minute,
+    month,
+    parse_date,
+    parse_date_delta,
+    second,
+    serialize_date,
+    serialize_date_delta,
+    timedelta_to_seconds,
+    week,
+    year
+)
+from webob.request import BaseRequest, LegacyRequest, Request
+from webob.response import Response
 from webob.util import html_escape
 
 __all__ = [
