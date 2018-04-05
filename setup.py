1f146c006489f6915adcd4500e67c36ed12e807d
Bert JW Regeer <bertjw@regeer.org>
2018-04-04 20:07:01 -0600

    Prep for 1.8.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ docs_extras = [
 
 setup(
     name='WebOb',
-    version='1.8.0rc1',
+    version='1.8.0',
     description="WSGI request and response object",
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
