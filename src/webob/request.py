fbbad16c4f2bd90821f6749d5224a4dd17b586d5
Bert JW Regeer <bertjw@regeer.org>
2018-11-14 23:18:43 -0700

    Goodbye FakeCGIBody

--- src/webob/request.py
+++ src/webob/request.py
@@ -1581,59 +1581,6 @@ def _cgi_FieldStorage__repr__patch(self):
 cgi_FieldStorage.__repr__ = _cgi_FieldStorage__repr__patch
 
 
-class FakeCGIBody(io.RawIOBase):
-    def __init__(self, vars, content_type):
-        warnings.warn(
-            "FakeCGIBody is no longer used by WebOb and will be removed from a future "
-            "version of WebOb. If you require FakeCGIBody please make a copy into "
-            "you own project",
-            DeprecationWarning,
-        )
-
-        if content_type.startswith("multipart/form-data"):
-            if not _get_multipart_boundary(content_type):
-                raise ValueError(
-                    "Content-type: %r does not contain boundary" % content_type
-                )
-        self.vars = vars
-        self.content_type = content_type
-        self.file = None
-
-    def __repr__(self):
-        inner = repr(self.vars)
-        if len(inner) > 20:
-            inner = inner[:15] + "..." + inner[-5:]
-        return "<%s at 0x%x viewing %s>" % (
-            self.__class__.__name__,
-            abs(id(self)),
-            inner,
-        )
-
-    def fileno(self):
-        return None
-
-    @staticmethod
-    def readable():
-        return True
-
-    def readinto(self, buff):
-        if self.file is None:
-            if self.content_type.startswith("application/x-www-form-urlencoded"):
-                data = "&".join(
-                    "%s=%s"
-                    % (quote_plus(bytes_(k, "utf8")), quote_plus(bytes_(v, "utf8")))
-                    for k, v in self.vars.items()
-                )
-                self.file = io.BytesIO(bytes_(data))
-            elif self.content_type.startswith("multipart/form-data"):
-                self.file = _encode_multipart(
-                    self.vars.items(), self.content_type, fout=io.BytesIO()
-                )[1]
-                self.file.seek(0)
-            else:
-                assert 0, "Bad content type: %r" % self.content_type
-        return self.file.readinto(buff)
-
 
 def _get_multipart_boundary(ctype):
     m = re.search(r"boundary=([^ ]+)", ctype, re.I)
