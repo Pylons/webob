6e354dd9471ff7acaa87b2031f797e7b8694745f
Bert JW Regeer <bertjw@regeer.org>
2020-01-21 22:46:35 -0800

    s/isAlive/is_alive/

--- tests/conftest.py
+++ tests/conftest.py
@@ -62,7 +62,7 @@ def serve():
             log.debug("shutting server down")
             server.shutdown()
             worker.join(1)
-            if worker.isAlive():
+            if worker.is_alive():
                 log.warning("worker is hanged")
             else:
                 log.debug("server stopped")
