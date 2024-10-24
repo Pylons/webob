5216c0db6fc31f18d5ee892afffbb4bc05e73403
Delta Regeer <bertjw@regeer.org>
2024-10-23 21:17:32 -0600

    Add info for 1.8.9 fix for Python 3.13

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ docs_extras = [
 
 setup(
     name='WebOb',
-    version='1.8.8',
+    version='1.8.9',
     description="WSGI request and response object",
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
